c68c66b1d3e99d0ee1ef4ee58198e856ef79cf6c
Usman Zia <u_zia@uncg.edu>
2024-02-06 12:12:25 -0500

    set up Java Spring backend

---
+++ backend/CampusMarketplace/src/main/java/com/example/marketplace/CampusMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusMarketplaceApplication.class, args);
+	}
+
+}
